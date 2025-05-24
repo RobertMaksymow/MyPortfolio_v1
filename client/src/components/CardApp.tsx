d4ea8f8cc675f5b872439c384b8b19e1f30979db
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-05-24 20:02:03 +0100

    Adding image to the portfolio card

--- client/src/components/CardApp.tsx
+++ client/src/components/CardApp.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import img from "../assets/img/guy.jpg";
 
 const CardApp = () => {
   return (
     <div className="card-app">
       <h2>Card-App</h2>
+      <picture>
+        <img src={img} alt="Description of image" />
+      </picture>
       <p>
         This is the Card-App component. Lorem ipsum dolor, sit amet consectetur
         adipisicing elit. Explicabo nihil minima ex minus illum obcaecati id
